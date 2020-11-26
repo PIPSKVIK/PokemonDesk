a71a4289996f578ede51e1e9afa2384af51ca1dd
Alexander <52704151+PIPSKVIK@users.noreply.github.com>
2020-11-26 21:42:07 +0300

    Добавляет несколько функций отлено добавленных в модули

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
-import Pokemon from './modules/pokemon.js'
-import random from './modules/randon.js'
+import Pokemon from './modules/pokemon.js';
+import random from './modules/randon.js';
+import renderLogs from './modules/renderLogs.js';
+import clickListener from './modules/clickListener.js';
+import renderButtonСountdown from './modules/renderButtonCountdown.js';
 
 function $querySelector (value) {
   return document.querySelector(value)
 }
 const $btn = $querySelector('#btn-kick');
 const $btnVolt = $querySelector('#btn-volt');
-const $logs = $querySelector('#logs');
 const $btnRenderSkillJolt = $querySelector('.button__count-skill');
 const $btnRenderSkillHit = $querySelector('.button__volt-count');
 $btnVolt.disabled = true;
@@ -15,6 +17,12 @@ const randomNumber = 12;
 const critHitNumber = 30;
 const thunderJoltMaxClick = 12;
 const criticalHitMexClick = 3;
+
+const renderButtonСountdownJolt = renderButtonСountdown(thunderJoltMaxClick, $btnRenderSkillJolt);
+const renderButtonCountdownHit = renderButtonСountdown(criticalHitMexClick, $btnRenderSkillHit);
+
+const countListenerJolt = clickListener();
+const countListenerHit = clickListener();
 
 
 //********** Герои **********
@@ -32,35 +40,6 @@ const player2 = new Pokemon ({
   hp: 100,
   selectors: 'enemy'
 })
-
-function clickListener () {
-  let counter = 0;
-
-  return (kickLimit = 100, btn) => {
-    counter++;
-    console.log(`click: ${counter}`);
-
-    if (counter >= kickLimit) {
-      console.log('STOP');
-      btn.disabled = true;
-    }
-  }
-}
-
-function renderButtonСountdown (number, element) {
-  let count = number;
-  return function () {
-    count -= 1;
-    element.innerText = `${count}`;
-  }
-}
-
-
-const renderButtonСountdownJolt = renderButtonСountdown(thunderJoltMaxClick, $btnRenderSkillJolt);
-const renderButtonCountdownHit = renderButtonСountdown(criticalHitMexClick, $btnRenderSkillHit);
-
-const countListenerJolt = clickListener();
-const countListenerHit = clickListener();
 
 // *********** События ***********
 $btn.addEventListener('click', function () {
@@ -108,72 +87,56 @@ $btnVolt.addEventListener('click', function () {
 
 function generateLog (firstPerson, secondPerson, demage) {
   const logs = [
-  `[${firstPerson.name}] вспомнил что-то важное, но неожиданно
-  [${secondPerson.name}], не помня себя от испуга, ударил в предплечье врага.
-  [${firstPerson.name}] получил -[${demage}] HP
-  [${firstPerson.hp.current} / ${firstPerson.hp.total}]`,
+    `[${firstPerson.name}] вспомнил что-то важное, но неожиданно
+    [${secondPerson.name}], не помня себя от испуга, ударил в предплечье врага.
+    [${firstPerson.name}] получил -[${demage}] HP
+    [${firstPerson.hp.current} / ${firstPerson.hp.total}]`,
 
-  `[${firstPerson.name}] поперхнулся, и за это
-  [${secondPerson.name}] с испугу приложил прямой удар коленом в лоб врага.
-  [${firstPerson.name}] получил -[${demage}] HP
-  [${firstPerson.hp.current} / ${firstPerson.hp.total}]`,
+    `[${firstPerson.name}] поперхнулся, и за это
+    [${secondPerson.name}] с испугу приложил прямой удар коленом в лоб врага.
+    [${firstPerson.name}] получил -[${demage}] HP
+    [${firstPerson.hp.current} / ${firstPerson.hp.total}]`,
 
-  `[${firstPerson.name}] забылся, но в это время наглый
-  [${secondPerson.name}], приняв волевое решение, неслышно подойдя сзади, ударил.
-  [${firstPerson.name}] получил -[${demage}] HP
-  [${firstPerson.hp.current} / ${firstPerson.hp.total}]`,
+    `[${firstPerson.name}] забылся, но в это время наглый
+    [${secondPerson.name}], приняв волевое решение, неслышно подойдя сзади, ударил.
+    [${firstPerson.name}] получил -[${demage}] HP
+    [${firstPerson.hp.current} / ${firstPerson.hp.total}]`,
 
-  `[${firstPerson.name}] пришел в себя, но неожиданно
-  [${secondPerson.name}] случайно нанес мощнейший удар.
-  [${firstPerson.name}] получил -[${demage}] HP
-  [${firstPerson.hp.current} / ${firstPerson.hp.total}]`,
+    `[${firstPerson.name}] пришел в себя, но неожиданно
+    [${secondPerson.name}] случайно нанес мощнейший удар.
+    [${firstPerson.name}] получил -[${demage}] HP
+    [${firstPerson.hp.current} / ${firstPerson.hp.total}]`,
 
-  `[${firstPerson.name}] поперхнулся, но в это время
-  [${secondPerson.name}] нехотя раздробил кулаком \<вырезанно цензурой\> противника.
-  [${firstPerson.name}] получил -[${demage}] HP
-  [${firstPerson.hp.current} / ${firstPerson.hp.total}]`,
+    `[${firstPerson.name}] поперхнулся, но в это время
+    [${secondPerson.name}] нехотя раздробил кулаком \<вырезанно цензурой\> противника.
+    [${firstPerson.name}] получил -[${demage}] HP
+    [${firstPerson.hp.current} / ${firstPerson.hp.total}]`,
 
-  `[${firstPerson.name}] удивился, а
-  [${secondPerson.name}] пошатнувшись влепил подлый удар.
-  [${firstPerson.name}] получил -[${demage}] HP
-  [${firstPerson.hp.current} / ${firstPerson.hp.total}]`,
+    `[${firstPerson.name}] удивился, а
+    [${secondPerson.name}] пошатнувшись влепил подлый удар.
+    [${firstPerson.name}] получил -[${demage}] HP
+    [${firstPerson.hp.current} / ${firstPerson.hp.total}]`,
 
-  `[${firstPerson.name}] высморкался, но неожиданно
-  [${secondPerson.name}]провел дробящий удар.
-  [${firstPerson.name}] получил -[${demage}] HP
-  [${firstPerson.hp.current} / ${firstPerson.hp.total}]`,
+    `[${firstPerson.name}] высморкался, но неожиданно
+    [${secondPerson.name}]провел дробящий удар.
+    [${firstPerson.name}] получил -[${demage}] HP
+    [${firstPerson.hp.current} / ${firstPerson.hp.total}]`,
 
-  `[${firstPerson.name}] пошатнулся, и внезапно наглый
-  [${secondPerson.name}] беспричинно ударил в ногу противника
-  [${firstPerson.name}] получил -[${demage}] HP
-  [${firstPerson.hp.current} / ${firstPerson.hp.total}]`,
+    `[${firstPerson.name}] пошатнулся, и внезапно наглый
+    [${secondPerson.name}] беспричинно ударил в ногу противника
+    [${firstPerson.name}] получил -[${demage}] HP
+    [${firstPerson.hp.current} / ${firstPerson.hp.total}]`,
 
-  `[${firstPerson.name}] расстроился, как вдруг, неожиданно
-  [${secondPerson.name}] случайно влепил стопой в живот соперника.
-  [${firstPerson.name}] получил -[${demage}] HP
-  [${firstPerson.hp.current} / ${firstPerson.hp.total}]`,
+    `[${firstPerson.name}] расстроился, как вдруг, неожиданно
+    [${secondPerson.name}] случайно влепил стопой в живот соперника.
+    [${firstPerson.name}] получил -[${demage}] HP
+    [${firstPerson.hp.current} / ${firstPerson.hp.total}]`,
 
-  `[${firstPerson.name}] пытался что-то сказать, но вдруг, неожиданно
-  [${secondPerson.name}] со скуки, разбил бровь сопернику.
-  [${firstPerson.name}] получил -[${demage}] HP
-  [${firstPerson.hp.current} / ${firstPerson.hp.total}]`
-];
+    `[${firstPerson.name}] пытался что-то сказать, но вдруг, неожиданно
+    [${secondPerson.name}] со скуки, разбил бровь сопернику.
+    [${firstPerson.name}] получил -[${demage}] HP
+    [${firstPerson.hp.current} / ${firstPerson.hp.total}]`
+  ];
 
   return logs[random(logs.length) - 1];
-}
-
-
-// function random (num) {
-//   return Math.ceil(Math.random() * num);
-// }
-
-// ************ render logs ************
-
-function renderLogs (log) {
-  for (let i = 0; i < 1; i++) {
-    const $p = document.createElement('p');
-    $p.classList.add('logs__text')
-    $p.innerText = `${log}`;
-    $logs.insertBefore($p, $logs.children[0]);
-  }
 }
